2f87461227a4b3e311ad4dec927562bc6ac45618
martynas3113 <martynas.pazusis@gmail.com>
2021-02-08 16:55:16 +0200

    firebase set up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,24 @@ import 'firebase/firestore';
 import 'firebase/auth';
 import './App.css';
 
+import { authState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  
+    apiKey: "AIzaSyDU-ulc4NE9tXSPqyDGQLl46DWfT249fsE",
+    authDomain: "react-chat-app-1b2ee.firebaseapp.com",
+    projectId: "react-chat-app-1b2ee",
+    storageBucket: "react-chat-app-1b2ee.appspot.com",
+    messagingSenderId: "774718209762",
+    appId: "1:774718209762:web:e4da0e50397281dc93e8bb",
+    measurementId: "G-QE073NDDW9"
+  
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
   return (
     <div className="App">
